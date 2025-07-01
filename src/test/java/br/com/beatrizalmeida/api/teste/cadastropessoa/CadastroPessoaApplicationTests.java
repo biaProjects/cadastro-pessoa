70782cd9ca0fbf620750cf31a5039f7eea120612
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-07-01 16:41:13 -0300

    fix/project files correctly added

---
+++ src/test/java/br/com/beatrizalmeida/api/teste/cadastropessoa/CadastroPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beatrizalmeida.api.teste.cadastropessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
